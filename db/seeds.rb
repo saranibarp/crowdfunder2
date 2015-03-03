364fc477c91d4948b935340267d1c2da91d9f33a
Maria Aranibar <saranibarp@outlook.com>
2015-03-03 22:36:00 +0000

    Added Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Crowdfunder seed data
+ 
+puts 'Deleting any existing data...'
+[Pledge, FundingLevel, Project].each { |model| model.destroy_all }
+ 
+puts 'Adding data...'
+nanny = Project.create(
+                  name: 'Robot Nanny',
+                  description: "This robot nanny will care for your kids, your pets and even you!\n\nRequires very little maintenance.\n\nPledge and help us build the future!")
+ 
+nanny.funding_levels.create(reward_name: 'Robot Nanny - Purple', amount: 100)
+nanny.funding_levels.create(reward_name: 'Robot Nanny - Pink', amount: 120)
+nanny.funding_levels.create(reward_name: 'Robot Nanny - Collector’s Edition', amount: 250)
+ 
+ 
+jetpack = Project.create(
+                  name: 'Personal Jetpack',
+                  description: "After four years of messing around in our basements, we're finally ready to start production on our Personal Jetback 3000.\n\nOur goal is to raise $100,000,000 to build our factory, get government approval and patent our technology.")
+ 
+jetpack.funding_levels.create(reward_name: 'T-shirt', amount: 10)
+jetpack.funding_levels.create(reward_name: 'T-shirt plus Coffee Table Book', amount: 100)
+jetpack.funding_levels.create(reward_name: 'Personal Jetpack plus Helmet', amount: 1000)
+ 
+names = ['Christina', 'Elder', 'Evelyn', 'Henrique', 'Jéssica', 'Kerene', 'Lucas', 'Lucas', 'Maria', 'Melissa', 'Mike', 'Raphael', 'Ronaldo', 'Steve', 'Thiago', 'Vance']
+ 
+funding_level_ids = FundingLevel.pluck(:id)
+ 
+names.each do |name|
+  Pledge.create(
+            name: name,
+            email: "#{name.downcase}@example.com",
+            funding_level_id: funding_level_ids.sample)
+end
